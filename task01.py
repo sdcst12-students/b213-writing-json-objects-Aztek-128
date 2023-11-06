e94fe217e7ec685a8e0c9bb76d7f12e46cc2bcb9
Aztek-128 <124203485+Aztek-128@users.noreply.github.com>
2023-11-06 12:19:02 -0800

    yes

--- task01.py
+++ task01.py
@@ -62,3 +62,72 @@ Enter in the scores for 10 students for Assignment 2:
 """
 
 import json
+import os
+
+class main():
+
+    def __init__(self) -> None:
+        with open('data.csv','r+') as file:
+            data = file.read()
+            if data == None:
+                file.write(json.dumps('{"0":"0"}'))
+                print("funny")
+        self.DataStart()
+
+    def DataStart(self):
+        choice = int(input("Hello there, what would you like to do: \n1) Create Assignment    \n2) Enter in an Edit   \n3) Delete an Edit   \n4) View Assignment    \n5) Do Nothing\nselect:    "))
+        if choice == 1:
+            self.Create()
+        if choice == 2:
+            self.Edit()
+        if choice == 3:
+            self.Delete()
+        if choice == 4:
+            self.View
+
+    def Create(self):
+        Assignment = str(input("What is your Assignment Name:    ")).capitalize()
+        amount = int(input("enter the amount of students:   "))
+        with open('data.csv','r') as file:
+            data = file.read()
+        with open('data.csv','w') as file:
+            sent = json.loads(data)
+            sent[f"{Assignment}"] = {}
+            for i in range(amount):
+                name = input("What is the Students Name:   ").capitalize()
+                mark = input(f"what is {name}'s mark:   ")
+                sent[f"{Assignment}"][f"{name}"] = f"{mark}"
+            file.write(json.dumps(sent))
+            print("success")    
+            
+            
+
+
+    def Edit(self):
+        with open('data.csv','r+') as file:
+            data = file.read()
+            sent = json.loads(data)
+            list_of_assignments = [i for i in sent if i != 0]
+            print(f"which assignment do you want to change: {list_of_assignments}")
+            chosen_assignment = str(input("What assignment do you want to change:   ")).capitalize()
+            list_of_students = [i for i in sent[chosen_assignment]]
+            student = str(input(f"Which student do you want to change the mark of: {list_of_students}:  ")).capitalize()
+            new_mark = int(input(f"what is {student}'s new mark:    "))
+            sent[f"{chosen_assignment}"][f"{student}"] = f"{new_mark}"
+            file.write(json.dumps(sent))
+            print("it works")
+
+            
+        
+    def AbeKopis(self):
+
+        pass
+    def Delete(self):
+        os.remove('data.csv')
+        with open('data.csv','w') as file:
+            file.write('{"0":"0"}')
+
+        pass
+    def View(self):
+        pass
+main()
